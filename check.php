fc56949e0986cadbd7bc9e74ae3e019d9d6b1df8
佐藤良太 <sato.ryota.2008@gmail.com>
2019-08-28 16:50:06 +0900

    ユーザー名が入力されていません を追加

--- check.php
+++ check.php
@@ -1,6 +1,28 @@
 <?php
 // スーパーグローバル変数(PHPが元々用意している変数)
-var_dump($_POST);
+
+// 送信されてきた値の取得
+$username = $_POST['username'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+
+// ユーザー名が空かチェック
+if ($username == '') {
+    $usernameResult ='ユーザー名が入力されていません。';
+} else {
+    $usernameResult = $username;
+}
+if ($email == '') {
+    $emailResult ='emailが入力されていません。';
+} else {
+    $emailResult = $email;
+}
+if ($content == '') {
+    $contentResult ='内容が入力されていません。';
+} else {
+    $contentResult = $content;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,9 +35,9 @@ var_dump($_POST);
 <body>
     <h1>入力内容確認</h1>
 
-    <p>名前：<?php echo $_POST["username"]; ?></p>
-    <p>メールアドレス：<?php echo $_POST["email"]; ?></p>
-    <p>内容：<?php echo $_POST["content"]; ?></p>
+    <p>名前：<?php echo $usernameResult; ?></p>
+    <p>メールアドレス：<?php echo $emailResult; ?></p>
+    <p>内容：<?php echo $contentResult; ?></p>
 
     <form action="index.html">
         <button>戻る</button>

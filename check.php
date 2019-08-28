cdc467727b813b5b1e8d2b99bac3a9f2d612b358
佐藤良太 <sato.ryota.2008@gmail.com>
2019-08-28 16:08:21 +0900

    a

---
+++ check.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>入力内容確認</title>
+</head>
+<body>
+    <h1>入力内容確認</h1>
+
+    <p>名前：○○</p>
+    <p>メールアドレス：○○@○○</p>
+
+    <form action="">
+        <button>戻る</button>
+        <input type="submit" value="OK">
+    </form>
+    
+</body>
+</html>

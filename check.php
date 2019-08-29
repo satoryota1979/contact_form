e381e11776677dbe87fd759e1667edd60e8074d9
佐藤良太 <sato.ryota.2008@gmail.com>
2019-08-29 14:27:21 +0900

    エスケープ処理入れた

--- check.php
+++ check.php
@@ -2,9 +2,16 @@
 // スーパーグローバル変数(PHPが元々用意している変数)
 
 // 送信されてきた値の取得
-$username = $_POST['username'];
-$email = $_POST['email'];
-$content = $_POST['content'];
+// XSS（クロスサイトスクリプティング）の対策をする
+
+// エスケープ処理:htmlspecialchars
+// htmlspecialchars(対象の文字,オプション,文字コード)
+$username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
+$email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+$content = htmlspecialchars($_POST['content'], ENT_QUOTES, 'UTF-8');
+
+
+// 入力無しエラー判定用、エラーの場合１
 $usernameError = 0;
 $emailError = 0;
 $contentError = 0;
@@ -39,14 +46,16 @@ if ($content == '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>入力内容確認</title>
 </head>
 <body>
-<div class="border col-5 ml-3 mt-3">
+<div class="border col-12">
     <h1>入力内容確認</h1>
     <div class="col-12 mb-5 border">
-        <p class="border">名前：<?php echo $usernameResult; ?></p>
+        <p>名前：<?php echo $usernameResult; ?></p>
         <p>メールアドレス：<?php echo $emailResult; ?></p>
         <p>内容：<?php echo $contentResult; ?></p>
 
@@ -56,6 +65,13 @@ if ($content == '') {
         </form>
     </div>
 </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 
 <?php
     if ($usernameError == 1) {
